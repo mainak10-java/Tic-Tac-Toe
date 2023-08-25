338ae9016b605ee4a62692245a076a13cfa03831
mainak10-java <mbmainak75@gmail.com>
2023-08-25 23:53:34 +0530

    Added HTML,CSS codes and JS functionalities

---
+++ script.js
@@ -0,0 +1,104 @@
+const newGameBtn= document.querySelector('.btn');
+const gameInfo= document.querySelector('.game-info');
+const boxes= document.querySelectorAll('.box');
+
+let currentPlayer;
+let gameGrid;
+
+let winningPositions=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+function initGame(){
+    currentPlayer="X";
+    gameGrid=["","","","","","","","",""];
+
+    //Once the game is over
+    boxes.forEach((box, index) =>{
+        box.innerText="";
+        boxes[index].style.pointerEvents="all";
+        box.classList = `box box${index+1}`;
+    })
+
+    newGameBtn.classList.remove("active");
+    gameInfo.innerText=`Current Player - ${currentPlayer}`;
+}
+
+initGame();
+
+function swapTurn(){
+    if(currentPlayer === 'X')
+     currentPlayer='O';
+    else
+     currentPlayer='X';
+
+    gameInfo.innerText=`Current Player - ${currentPlayer}`;
+}
+
+function checkWhoWon(){
+    let answer="";
+
+    winningPositions.forEach((position) => {
+        if((gameGrid[position[0]]!== "" || gameGrid[position[1]]!== "" || gameGrid[position[2]]!== "") 
+        && (gameGrid[position[0]] === gameGrid[position[1]]) && (gameGrid[position[1]] === gameGrid[position[2]])){
+
+            if(gameGrid[position[0]] ==='X')
+                answer='X';
+            else
+                answer='O';
+        
+        
+        boxes.forEach((box) =>{
+            box.style.pointerEvents='none';
+        }) 
+
+        boxes[position[0]].classList.add('win');
+        boxes[position[1]].classList.add('win');
+        boxes[position[2]].classList.add('win');
+
+        }   
+    });
+
+    if(answer !== ""){
+        gameInfo.innerText= `Winner is ${answer}`;
+        newGameBtn.classList.add('active');
+    }
+
+    let fillCount=0;
+    gameGrid.forEach((box) =>{
+        if(box !== "")
+         fillCount++;
+    });
+
+    if(fillCount === 9){
+        gameInfo.innerText=`Game Tied`;
+        newGameBtn.classList.add('active');
+    }
+}
+
+function handleClick(index){
+    if(gameGrid[index] === "")
+    {
+        boxes[index].innerText=currentPlayer;
+        gameGrid[index]=currentPlayer;
+        boxes[index].style.pointerEvents= "none";
+
+        swapTurn();
+        checkWhoWon();
+    }
+}
+
+boxes.forEach((box, index) =>{
+    box.addEventListener('click', () =>{
+        handleClick(index);
+    })
+});
+
+newGameBtn.addEventListener('click', initGame);
